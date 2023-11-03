12fb559fbd24ba6ee531ec8b5d0935e90c89c28c
PrangonChakraborty-1805085 <1805085@ugrad.cse.buet.ac.bd>
2023-11-03 15:18:40 +0600

    updated page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,7 @@
 "use client";
-import Decrement from "../components/Decrement";
-import Increment from "../components/Increment";
 import { useSelector } from "react-redux";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 export default function Home() {
-  // const [Name, setName] = useState("");
-  const name = useSelector((state) => {
-    return state.persistedAuthReducer.value.user;
-  });
-  console.log("name is ", name);
-  return (
-    <div>
-      <h1>user is : {name}</h1>
-      <Increment />
-      <Decrement />
-    </div>
-  );
+  return <div> home</div>;
 }
